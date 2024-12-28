523acd6bfd07c8c1f1cdf269ce65e51e201b708b
anisanhy <joyabrianrantepasang14@gmail.com>
2024-12-29 00:15:15 +0800

    create menu scene

---
+++ Assets/Scripts/UI/SceneManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+}
